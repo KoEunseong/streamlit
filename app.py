1889861d550332f69e51c346ab0fc40170d436e0
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-06-17 02:29:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,12 @@ import matplotlib.pyplot as plt
 st.title("OSMnx 도로 네트워크 시각화")
 
 # 사용자로부터 도시 이름 입력 받기
-city_name = st.text_input("도시 이름을 입력하세요", "New York, USA")
+city_name = New York, USA
+graph = ox.graph_from_place(city_name, network_type="all")
 
-# OSMnx를 사용하여 도로 네트워크 가져오기
-try:
-    graph = ox.graph_from_place(city_name, network_type="all")
-    st.success("도로 네트워크 데이터 로드 완료!")
-except:
-    st.error("도로 네트워크 데이터를 로드하는 중 오류가 발생했습니다.")
 
-# 도로 네트워크 시각화
-if graph is not None:
-    # 도로 네트워크 그래프를 Matplotlib Figure로 변환
-    fig, ax = ox.plot_graph(graph, show=False, close=False)
+    # # 도로 네트워크 그래프를 Matplotlib Figure로 변환
+    # fig, ax = ox.plot_graph(graph, show=False, close=False)
 
-    # Matplotlib Figure를 Streamlit에 표시
-    st.pyplot(fig)
+    # # Matplotlib Figure를 Streamlit에 표시
+st.pyplot(fig)
