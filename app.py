4285252ac7314247365f9335742e249c90d50cd3
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-06-17 02:30:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 # 페이지 제목 설정
 st.title("OSMnx 도로 네트워크 시각화")
 
-# 사용자로부터 도시 이름 입력 받기
-city_name = New York, USA
-graph = ox.graph_from_place(city_name, network_type="all")
+# # 사용자로부터 도시 이름 입력 받기
+# city_name = New York, USA
+# graph = ox.graph_from_place(city_name, network_type="all")
 
 
-    # # 도로 네트워크 그래프를 Matplotlib Figure로 변환
-    # fig, ax = ox.plot_graph(graph, show=False, close=False)
+#     # # 도로 네트워크 그래프를 Matplotlib Figure로 변환
+#     # fig, ax = ox.plot_graph(graph, show=False, close=False)
 
-    # # Matplotlib Figure를 Streamlit에 표시
-st.pyplot(fig)
+#     # # Matplotlib Figure를 Streamlit에 표시
+# st.pyplot(fig)
