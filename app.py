5e6762d6418d9ad66ca548e565c2ac5eb8440907
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-06-17 02:17:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import osmnx as ox
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame(
-    np.random.randn(10, 2),
-    columns=['x', 'y'])
-st.line_chart(df)
+# df = pd.DataFrame(
+#     np.random.randn(10, 2),
+#     columns=['x', 'y'])
+# st.line_chart(df)
